879debd3f46a78cf536d11db79a81c4006acd008
zlx454545 <zlx454545@gmail.com>
2023-11-06 12:07:53 +0900

    styles: color 전역 변수 설정

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,6 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+:root{
+  /* Main color */
+  --color-M_01: #020202;
+  --color-M_02: #757575
+  --color-M_03: #959595
+  --color-M_04: #D2D2D2
+  
+  /* Secondary color */
+  --color-S_01: #FFC43D
+  --color-S_02: #F72585
+  --color-S_03: #7209B7
+  --color-S_04: #1BCCBA
+}
+
 *,
 *::before,
 *::after {
