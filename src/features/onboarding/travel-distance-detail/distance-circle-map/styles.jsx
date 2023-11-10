d4951c8eed755bff1e1e3b12e48a462aebc8be54
zlx454545 <zlx454545@gmail.com>
2023-11-10 17:25:09 +0900

    rename: map 폴더 distance-circle-map 폴더로 변경

--- src/features/onboarding/travel-distance-detail/distance-circle-map/styles.jsx
+++ src/features/onboarding/travel-distance-detail/distance-circle-map/styles.jsx
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 
 export const MapContainer = styled.div`
   height: 23.2rem;
-  margin-top: -44px;
+  margin-top: -4.4rem;
   margin-left: -2rem;
   margin-right: -2rem;
   overflow-x: hidden;
