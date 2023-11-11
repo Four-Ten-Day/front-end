8a0e3b4c974612f5584d23a894f5e5b847623a40
zlx454545 <zlx454545@gmail.com>
2023-11-11 09:38:20 +0900

    feat: home 배경화면 애니메이션 적용

--- src/ui/home-background-image/index.jsx
+++ src/ui/home-background-image/index.jsx
@@ -1,7 +1,49 @@
-import { createPortal } from 'react-dom';
-import * as S from './styles';
+// import { createPortal } from 'react-dom';
+// import * as S from './styles';
 
-function HomeBackgroundImage() {
-  return createPortal(<S.HomeBackgroundImage />, document.body);
-}
-export default HomeBackgroundImage;
+// function HomeBackgroundImage() {
+//   return createPortal(<S.HomeBackgroundImage />, document.body);
+// }
+// export default HomeBackgroundImage;
+
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
+import styled from 'styled-components';
+import animationData from '../../../public/home-animation.json';
+import { createPortal } from 'react-dom';
+
+// 스타일드 컴포넌트 생성
+const StyledAnimationContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  overflow: hidden;
+`;
+
+const LottieAnimation = () => {
+  const animationContainer = useRef(null);
+
+  useEffect(() => {
+    const anim = lottie.loadAnimation({
+      container: animationContainer.current,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      animationData: animationData,
+    });
+
+    anim.setSpeed(0.5);
+
+    return () => anim.destroy();
+  }, []);
+
+  return createPortal(
+    <StyledAnimationContainer ref={animationContainer} />,
+    document.body
+  );
+};
+
+export default LottieAnimation;
