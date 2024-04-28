9c9f0da69d7174b9cda0dfa8e7fd5b0ce1825568
zlx454545 <zlx454545@gmail.com>
2024-04-28 18:36:06 +0900

    chore: tailwind color palette 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'primary-01': '#020202',
+        'primary-02': '#757575',
+        'primary-03': '#959595',
+        'primary-04': '#D2D2D2',
+        'primary-05': '#F3F3F3',
+        'secondary-01': '#FFC43D',
+        'secondary-02': '#F72585',
+        'secondary-03': '#7209B7',
+        'secondary-04': '#1BCCBA',
       },
     },
   },
