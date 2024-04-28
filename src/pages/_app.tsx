f0a2d5e114847481467b1b06111eb45790df567c
zlx454545 <zlx454545@gmail.com>
2024-04-29 03:37:54 +0900

    feat: 랜딩 페이지 구현

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,28 @@
 import Layout from '@/components/layout';
 import '@/styles/globals.css';
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import { Jua } from 'next/font/google';
+import { ReactElement, ReactNode } from 'react';
 
 const jua = Jua({ subsets: ['latin'], weight: '400' });
 
-const App = ({ Component, pageProps }: AppProps) => {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <Layout>
-      <main className={jua.className}>
-        <Component {...pageProps} />
-      </main>
+      <div className={jua.className}>
+        {getLayout(<Component {...pageProps} />)}
+      </div>
     </Layout>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,56 @@
-import LandingAnimation from '@/components/landing-animation';
+import type { NextPageWithLayout } from './_app';
+import { ReactElement } from 'react';
+import Image from 'next/image';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
 
-const Home = () => {
+const Lottie = dynamic(
+  () => import('react-lottie-player').then((mod) => mod.default),
+  {
+    ssr: false,
+  }
+);
+
+const Home: NextPageWithLayout = () => {
   return (
     <>
-      <LandingAnimation />
+      <Lottie
+        className="fixed bottom-0 left-1/2 -translate-x-1/2 -z-10 overflow-hidden "
+        path={'/landing-lottie.json'}
+        play={true}
+        loop={false}
+        aria-hidden={true}
+      />
+
+      <header className="flex justify-center">
+        <Image
+          src={'/logo.svg'}
+          alt="onol logo"
+          width={44}
+          height={40}
+          className="mb-20"
+          priority
+        />
+      </header>
+
+      <main className="flex flex-col gap-12 w-full">
+        <section>
+          <h1 className="flex flex-col gap-5 text-center text-h1">
+            <p>오늘 뭐하고 놀지 고민되나요?</p>
+            <p> 오놀이 추천해줄게요 :)</p>
+          </h1>
+        </section>
+
+        <nav className="text-center">
+          <Link href={'/'}>네 좋아요!</Link>
+        </nav>
+      </main>
     </>
   );
+};
+
+Home.getLayout = (page: ReactElement) => {
+  return <div className="flex flex-col items-center px-8 py-28">{page}</div>;
 };
 
 export default Home;
