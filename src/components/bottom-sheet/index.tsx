90bc8687d2040a30015f19ee41e0aae307b6811c
zlx454545 <zlx454545@gmail.com>
2024-04-30 13:43:04 +0900

    design: bottom sheet 컴포넌트 스타일 수정

--- src/components/bottom-sheet/index.tsx
+++ src/components/bottom-sheet/index.tsx
@@ -6,7 +6,7 @@ type BottomSheetProps = {
 
 const BottomSheet = ({ children }: BottomSheetProps) => {
   return (
-    <div className="fixed bottom-0 z-10 shadow-top-sm px-5 py-7 w-full xs:max-w-screen-xs">
+    <div className="fixed bottom-0 z-10 shadow-top-sm px-5 py-7 w-full xs:max-w-screen-xs bg-primary-06">
       {children}
     </div>
   );
