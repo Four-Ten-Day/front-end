d7cd21bbcf1b8ee290e46a61fe50049da91acdfa
zlx454545 <zlx454545@gmail.com>
2024-04-30 14:28:27 +0900

    feat: mode 페이지 구현

--- src/pages/mode/index.tsx
+++ src/pages/mode/index.tsx
@@ -2,15 +2,31 @@ import BottomSheet from '@/components/bottom-sheet';
 import { NextPageWithLayout } from '../_app';
 import Button from '@/components/button';
 import { ReactElement } from 'react';
+import ModeSelector from '@/features/onboarding/mode-selector';
+import Ellipsis from '@/components/ellipsis';
 
 const Mode: NextPageWithLayout = () => {
-  return <>mode page</>;
+  return (
+    <>
+      <h1 className="text-h1 text-center">오늘은 혼자 놀기? 같이 놀기?</h1>
+
+      <section className="flex gap-4">
+        <ModeSelector />
+      </section>
+    </>
+  );
 };
 
 Mode.getLayout = (page: ReactElement) => {
   return (
     <>
-      <div className="px-5">{page}</div>
+      {/* TODO: 추후 selector로 각 페이지별이 아닌 컴포넌트 하나로 분리하기 */}
+      <div className="mb-[72px]">
+        <Ellipsis current={1} total={3} />
+      </div>
+
+      <div className="flex flex-col gap-16 px-5">{page}</div>
+
       <BottomSheet>
         <Button href={'/'} size="L" variant="contained">
           선택완료
