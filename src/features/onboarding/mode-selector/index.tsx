22428c716aa7f8037efe37187ae280003d175d0e
zlx454545 <zlx454545@gmail.com>
2024-04-30 14:11:43 +0900

    feat: mode selector 컴포넌트 구현

---
+++ src/features/onboarding/mode-selector/index.tsx
@@ -0,0 +1,46 @@
+import FilterButton from '@/components/filter-button';
+import { Mode, modeState } from '@/store/onboarding';
+import Image from 'next/image';
+import { useRecoilState } from 'recoil';
+
+const ModeSelector = () => {
+  const [selectedMode, setSelectedMode] = useRecoilState(modeState);
+
+  const handleClick = (mode: Mode) => {
+    const newMode = mode === selectedMode ? null : mode;
+    setSelectedMode(newMode);
+  };
+
+  return (
+    <>
+      <FilterButton
+        size="L"
+        active={selectedMode === 'alone'}
+        onClick={() => handleClick('alone')}
+      >
+        <Image
+          src={'/images/mode-alone.svg'}
+          alt="혼자 놀기"
+          width={75}
+          height={76}
+        />
+        <span>혼자 놀거에요</span>
+      </FilterButton>
+      <FilterButton
+        size="L"
+        active={selectedMode === 'together'}
+        onClick={() => handleClick('together')}
+      >
+        <Image
+          src={'/images/mode-together.svg'}
+          alt="같이 놀기"
+          width={75}
+          height={76}
+        />
+        <span>같이 놀거에요</span>
+      </FilterButton>
+    </>
+  );
+};
+
+export default ModeSelector;
