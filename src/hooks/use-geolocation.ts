b86c6d7c67fa49840f91a0622023ff296b0d3092
zlx454545 <zlx454545@gmail.com>
2024-05-02 19:16:27 +0900

    fix: 공용 use geolocation 훅 수정

--- src/hooks/use-geolocation.ts
+++ src/hooks/use-geolocation.ts
@@ -1,43 +1,36 @@
+import { Position, positionState } from '@/store/distance/atom';
 import { useEffect, useState } from 'react';
-
-export type Position = {
-  lat: number;
-  lng: number;
-};
+import { useRecoilState } from 'recoil';
 
 const useGeoLocation = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const [position, setPosition] = useState<Position>({
-    lat: 33.5563,
-    lng: 126.79581,
-  });
-
-  const getPosition = () => {
-    if (!navigator.geolocation)
-      return setError('Your browser does not support geolocation');
-
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-        setIsLoading(false);
-      },
-      (error) => {
-        setError(error.message);
-        setIsLoading(false);
-      }
-    );
-  };
+  const [position, setPosition] = useRecoilState<Position>(positionState);
 
   useEffect(() => {
-    getPosition();
-  }, []);
+    const getPosition = () => {
+      if (!navigator.geolocation)
+        return setError('Your browser does not support geolocation');
 
-  return { isLoading, position, error, getPosition };
+      setIsLoading(true);
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          setPosition({
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude,
+          });
+          setIsLoading(false);
+        },
+        (error) => {
+          setError(error.message);
+          setIsLoading(false);
+        }
+      );
+    };
+    getPosition();
+  }, [setPosition]);
+
+  return { isLoading, position, error };
 };
 
 export default useGeoLocation;
