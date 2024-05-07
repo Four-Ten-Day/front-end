437ab921d84cf61885084f8d1d42d6b2ec66bb9e
zlx454545 <zlx454545@gmail.com>
2024-05-08 01:51:49 +0900

    fix: logo unoptimized prop 추가

--- src/components/features/home/logo/index.tsx
+++ src/components/features/home/logo/index.tsx
@@ -4,7 +4,14 @@ import * as S from './styles';
 export default function Logo() {
   return (
     <S.Header>
-      <Image src="/images/logo.svg" alt="오놀 로고" width={44} height={40} />
+      <Image
+        src={'/images/logo.svg'}
+        alt="오놀 로고"
+        width={44}
+        height={40}
+        unoptimized
+        priority
+      />
     </S.Header>
   );
 }
