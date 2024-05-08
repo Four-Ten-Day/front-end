acdfc2b16dbcb41acd9b759b310acf900396ca3a
zlx454545 <zlx454545@gmail.com>
2024-05-08 19:43:50 +0900

    feat: 접근성 향상을 위한 status role 추가

--- src/components/features/interest/interest-selector/index.tsx
+++ src/components/features/interest/interest-selector/index.tsx
@@ -8,6 +8,7 @@ import { isAllInterestsSelectedState } from '@/store/interest/selectors';
 import Image from 'next/image';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import * as S from './styles';
+import { VisuallyHidden } from '@/components/common-ui/visually-hidden';
 
 const InterestSelector = () => {
   const allInterests = useRecoilValue(allInterestsState);
@@ -34,17 +35,34 @@ const InterestSelector = () => {
     setSelectedInterests((prev) => [...prev, value]);
   };
 
+  const liveMessage = (() => {
+    let message = '';
+    if (!selectedInterests.length)
+      message = '어떤 기분을 느끼고 싶은지 선택해 보세요.';
+    else if (isAllSelcted) message = '모든 기분이 선택되었어요.';
+    else
+      message =
+        allInterests
+          .filter(({ value }) => selectedInterests.includes(value))
+          .map(({ label }) => label)
+          .join(' ') + '이 선택되었어요.';
+
+    return message;
+  })();
+
   return (
     <S.InterestSelector>
-      <FilterButton active={isAllSelcted} size="S" onClick={handleClickAll}>
-        <Image
-          src={'/images/interest-all.svg'}
-          alt="모두 선택"
-          width={24}
-          height={25}
-        />
+      <FilterButton
+        active={isAllSelcted}
+        size="S"
+        onClick={handleClickAll}
+        role="checkbox"
+        aria-checked={isAllSelcted}
+      >
+        <Image src={'/images/interest-all.svg'} alt="" width={24} height={25} />
         <span>전체 선택</span>
       </FilterButton>
+
       {allInterests.map(({ emojiPath, label, value }) => {
         const isSelected = selectedInterests.includes(value);
 
@@ -54,12 +72,15 @@ const InterestSelector = () => {
             size="S"
             key={value}
             onClick={() => handleClickInterest(value)}
+            role="checkbox"
+            aria-checked={isSelected}
           >
-            <Image src={emojiPath} alt={label} width={24} height={25} />
+            <Image src={emojiPath} alt="" width={24} height={25} />
             <span>{label}</span>
           </FilterButton>
         );
       })}
+      <VisuallyHidden role="status">{liveMessage}</VisuallyHidden>
     </S.InterestSelector>
   );
 };
