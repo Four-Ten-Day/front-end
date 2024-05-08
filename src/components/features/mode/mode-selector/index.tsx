ac142cdbbfc24a7f158881c9599ec01c3a940724
zlx454545 <zlx454545@gmail.com>
2024-05-08 16:51:08 +0900

    feat: mode selector 컴포넌트 접근성 개선

--- src/components/features/mode/mode-selector/index.tsx
+++ src/components/features/mode/mode-selector/index.tsx
@@ -3,6 +3,7 @@ import { ModeValue, selectedModeState } from '@/store/mode/atom';
 import Image from 'next/image';
 import { useRecoilState } from 'recoil';
 import * as S from './styles';
+import { VisuallyHidden } from '@/components/common-ui/visually-hidden';
 
 const ModeSelector = () => {
   const [selectedMode, setSelectedMode] = useRecoilState(selectedModeState);
@@ -12,35 +13,48 @@ const ModeSelector = () => {
     setSelectedMode(newMode);
   };
 
+  const liveMessage = (() => {
+    let message = '';
+    if (!selectedMode) message = '혼자 놀지 같이 놀지 선택해주세요.';
+
+    if (selectedMode === 'alone') message = '혼자 놀기를 선택했습니다.';
+    if (selectedMode === 'together') message = '같이 놀기를 선택했습니다.';
+
+    return message;
+  })();
+
   return (
-    <S.ModeSelector>
-      <FilterButton
-        size="L"
-        active={selectedMode === 'alone'}
-        onClick={() => handleClick('alone')}
-      >
-        <Image
-          src={'/images/mode-alone.svg'}
-          alt="혼자 놀기"
-          width={75}
-          height={76}
-        />
-        <span>혼자 놀거에요</span>
-      </FilterButton>
-      <FilterButton
-        size="L"
-        active={selectedMode === 'together'}
-        onClick={() => handleClick('together')}
-      >
-        <Image
-          src={'/images/mode-together.svg'}
-          alt="같이 놀기"
-          width={75}
-          height={76}
-        />
-        <span>같이 놀거에요</span>
-      </FilterButton>
-    </S.ModeSelector>
+    <>
+      <S.ModeSelector>
+        <FilterButton
+          size="L"
+          active={selectedMode === 'alone'}
+          onClick={() => handleClick('alone')}
+          role="checkbox"
+          aria-checked={selectedMode === 'alone'}
+        >
+          <Image src={'/images/mode-alone.svg'} alt="" width={75} height={76} />
+          <span>혼자 놀거에요</span>
+        </FilterButton>
+        <FilterButton
+          size="L"
+          active={selectedMode === 'together'}
+          onClick={() => handleClick('together')}
+          role="checkbox"
+          aria-checked={selectedMode === 'together'}
+        >
+          <Image
+            src={'/images/mode-together.svg'}
+            alt=""
+            width={75}
+            height={76}
+          />
+          <span>같이 놀거에요</span>
+        </FilterButton>
+      </S.ModeSelector>
+
+      <VisuallyHidden role="status">{liveMessage}</VisuallyHidden>
+    </>
   );
 };
 
