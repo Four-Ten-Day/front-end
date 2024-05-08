b83175dfcef9a82ac69983ffc4f46a50dd7ac8a4
zlx454545 <zlx454545@gmail.com>
2024-05-08 19:37:42 +0900

    feat: interest 페이지 title 및 meta 태그 추가

--- src/pages/interest/index.tsx
+++ src/pages/interest/index.tsx
@@ -6,12 +6,20 @@ import NextPageButton from '@/components/features/interest/next-page-button';
 import useRefreshRestore from '@/hooks/use-refresh-restore';
 import Layout from '@/components/features/interest/layout';
 import Header from '@/components/features/interest/header';
+import Head from 'next/head';
 
 const Interest: NextPageWithLayout = () => {
   useRefreshRestore();
 
   return (
     <>
+      <Head>
+        <title>오놀 | 어떤 기분을 느끼고 싶나요?</title>
+        <meta
+          name="description"
+          content="오늘은 어떤 컨셉으로 놀고 싶은지 정해보세요."
+        />
+      </Head>
       <Ellipsis current={2} total={3} />
       <Header />
       <InterestSelector />
