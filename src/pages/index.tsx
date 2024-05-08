7b8fba8d99b22854fb5f2e1a9ca6340ada8b3692
zlx454545 <zlx454545@gmail.com>
2024-05-08 16:03:19 +0900

    feat: 홈페이지 title 및 메타 태그 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,18 @@ import Logo from '@/components/features/home/logo';
 import MessageSection from '@/components/features/home/section';
 import Nav from '@/components/features/home/nav';
 import { Layout } from '@/components/features/home/layout';
+import Head from 'next/head';
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
+      <Head>
+        <title>오놀 | 오늘 뭐하고 놀지?</title>
+        <meta
+          name="description"
+          content="오늘 뭐하고 놀지 고민 되나요? 오놀이 랜덤으로 추천 해줄게요!"
+        />
+      </Head>
       <Logo />
       <MessageSection />
       <Nav />
