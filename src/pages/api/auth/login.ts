bbe70464e5dbd37d9b9349a63caa88651fff6a5a
zlx454545 <zlx454545@gmail.com>
2024-05-15 17:22:18 +0900

    fix: redirect uri 수정

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   const client_id = process.env.NEXT_PUBLIC_AUTH_GITHUB_ID;
-  const redirect_uri = 'http://localhost:3000/api/auth/callback/github';
+  const redirect_uri = process.env.NEXT_PUBLIC_REDIRECT_URI;
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}`;
 
   res.redirect(githubAuthUrl);
